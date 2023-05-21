d2b4226fd2c6a6582d52d685548ff217e3c0c5eb
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-05-21 13:53:07 +0700

    INIT - Account Service - Card Service - Loan Service

---
+++ loan-service/src/test/java/com/ngochung/loan/LoanServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ngochung.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
