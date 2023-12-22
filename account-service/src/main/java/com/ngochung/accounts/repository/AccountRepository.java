6c03cd5172a9612712a99df289f1766c3d984584
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-12-22 14:43:09 +0700

    clean

--- account-service/src/main/java/com/ngochung/accounts/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ngochung.accounts.repository;
-
-import com.ngochung.accounts.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-  Account findByCustomerId(long customerId);
-}
--- account-service/src/main/java/com/ngochung/accounts/service/AccountService.java
+++
@@ -1,7 +0,0 @@
-package com.ngochung.accounts.service;
-
-import com.ngochung.accounts.entity.Account;
-
-public interface AccountService {
-  Account getAccountByCustomer(long customerId);
-}
