b7336f2ebbc022820f4751489961587a41e88efd
NgocHung <hoang.ngoc.hungit@gmail.com>
2023-12-23 14:25:29 +0700

    Account Service | Add swagger ui

--- account/src/main/java/com/smartdev/account/AccountApplication.java
+++ account/src/main/java/com/smartdev/account/AccountApplication.java
@@ -1,13 +1,35 @@
 package com.smartdev.account;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Accounts microservice REST API Documentation",
+            description = "Bank Accounts microservice REST API Documentation",
+            version = "v1",
+            contact =
+                @Contact(
+                    name = "Smart Dev",
+                    email = "hoang.ngoc.hungit@gmail.com",
+                    url = "fb/NgocHungUSD"),
+            license = @License(name = "Apache 2.0", url = "https://www.eazybytes.com")),
+    externalDocs =
+        @ExternalDocumentation(
+            description = "Bank Accounts microservice REST API Documentation",
+            url = "fb/NgocHungUSD"))
 public class AccountApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AccountApplication.class, args);
+  }
 }
